c6cdbe5027acde8ac101b9626b4dd58b184a1c00
PrajwalCH <prajjwal2058@gmail.com>
2021-03-30 21:16:55 +0545

    refactor: remove extra ';' at the end of function startGame

--- src/game.c
+++ src/game.c
@@ -84,7 +84,7 @@ void startGame(void) {
   int totalStoredWords = sizeof(wordList) / sizeof(wordList[0]);
   shuffleWordList(wordList, totalStoredWords);
   presentWords(wordList, totalStoredWords - 1);
-};
+}
 
 void displayInstructions(void)
 {
